b62aff68561b486a179170a8a250fe9dbaf4bd83
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2022-09-22 08:17:52 +0530

    Configuring Client

--- MicroServiceCommunication.GRPCClient/Program.cs
+++ MicroServiceCommunication.GRPCClient/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using MicorServiceCommunication.GRPCClient;
+
+internal static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        string name = args.Length > 0 && String.IsNullOrWhiteSpace(args[0])
+        ? "GreeterClient"
+        : args[0];
+
+        using var channel = GrpcChannel.ForAddress("https://localhost:7082");
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(
+                          new HelloRequest { Name = name });
+        Console.WriteLine("Greeting: " + reply.Message);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
